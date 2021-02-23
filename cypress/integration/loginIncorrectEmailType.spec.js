624bfc0236858c76b5b38188b99302c20bcaaca4
iva.petkovic <ivapetkovic85@gmail.com>
2021-02-23 11:17:12 +0100

    login with incorrect email type file added

---
+++ cypress/integration/loginIncorrectEmailType.spec.js
@@ -0,0 +1,14 @@
+describe("Login test with incorrect email type", () => {
+    it('Visit gallery page', () => {
+        cy.visit('/')
+    })
+    it ("Click on login button", () => {
+        cy.get("a[href='/login']").click()
+    })    
+    it ('Login with incorrect email type', () => {
+        cy.get('#email').clear().type('ivatest1122')
+        cy.get('#password').clear().type('test1234')
+        cy.get('button').click()
+    })
+    
+})
